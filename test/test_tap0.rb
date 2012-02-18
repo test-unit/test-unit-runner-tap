8976cd8464109d50c540ae0344dfa7d37b692006
7rans <transfire@gmail.com>
2012-02-18 10:59:40 -0500

    Old test runner no longer needs prove command hack.

--- test/test_tap0.rb
+++ test/test_tap0.rb
@@ -4,7 +4,7 @@
 # in backtraces, it should be filtered out.
 
 require 'stringio'
-require 'test/unit/ui/tap/testrunner'
+require 'test/unit/ui/tap/perl_testrunner'
 
 class TestTap0 < Test::Unit::TestCase
   def test_run
@@ -17,8 +17,7 @@ class TestTap0 < Test::Unit::TestCase
       def test_fail; assert_equal(3, 1 - 2); end; fail_line = __LINE__
     end
     output = StringIO.new
-    runner = Test::Unit::UI::Tap::OldTestRunner.new(test_case.suite,
-                                                 :output => output)
+    runner = Test::Unit::UI::Tap::OldTestRunner.new(test_case.suite, :output => output)
     result = runner.start; start_line = __LINE__
     assert_equal(<<-EOR, output.string.gsub(/[\d\.]+ seconds/, "0.001 seconds"))
 1..2
@@ -26,7 +25,6 @@ not ok 1 - test_fail(): <3> expected but was
 # Failure:
 # test_fail()
 #     [#{__FILE__}:#{fail_line}:in `test_fail'
-#      #{File.expand_path(__FILE__+'/../..')}/lib/test/unit/ui/tap/testrunner.rb:134:in `start'
 #      #{__FILE__}:#{start_line}:in `test_run']:
 # <3> expected but was
 # <-1>.
@@ -34,7 +32,7 @@ ok 2 - test_success()
 # Finished in 0.001 seconds.
 # 2 tests, 2 assertions, 1 failures, 0 errors, 0 pendings, 0 omissions, 0 notifications
 EOR
-    assert_true(result.passed?)  # think this is incorrect
+    assert_false(result.passed?)
   end
 end
 
